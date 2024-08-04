10e1ca39da7f26a520cec6c1879e83cfd8a21962
neerajaaaa <chneerajaa@gmail.com>
2024-08-04 17:20:40 -0500

    checking squash3

--- src/main/java/com/jagrosh/jmusicbot/queue/AbstractQueue.java
+++ src/main/java/com/jagrosh/jmusicbot/queue/AbstractQueue.java
@@ -4,7 +4,8 @@
 package com.jagrosh.jmusicbot.queue;
 /*panja testing checking checking squash
 * S
-* adding 1 */
+* adding 1
+* adding 2*/
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
